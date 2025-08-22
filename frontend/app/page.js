69f46534ee2645df38a4c1a46fe4b5f8ec7329c3
qblitz <wiktorblaszek@gmail.com>
2025-08-22 02:29:55 +0200

    Added red bar on top and logo in header

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,13 +1,28 @@
 import ChatContainer from './components/ChatContainer';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="page">
+      <div className="red-bar">
+      <Image
+          src="/assets/info_icon.png"
+          alt="Icon"
+          width={40}
+          height={40}
+          className="icon"
+        />
+      </div>
       <header>
-        <h1>D&D Agent</h1>
-        <p>Ask a question about Dungeons & Dragons 5th Edition</p>
+        <Image
+        src="/assets/pbots_logo.png"          
+        alt="PBOTS Logo"
+        width={80}
+        height={80}
+        />
+        <h1>PBOTS</h1>
+        <p>Zadaj pytanie!</p>
       </header>
-      
       <ChatContainer />
     </div>
   );
